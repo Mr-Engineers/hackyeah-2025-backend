87c9a9b2760e0c43756271598715c03db6af3f26
bindasp <bindas.patryk@gmail.com>
2025-10-04 20:01:19 +0200

    updated file structure

--- app/player_state/router.py
+++ app/player_state/router.py
@@ -1,47 +1,25 @@
 from fastapi import APIRouter, HTTPException
+from app.player_state.schema import PlayerStateUpdate
 from app.player_state.models import PlayerState
-import redis
-import json
+from app.player_state.services import PlayerStateService
 
 router = APIRouter()
-
-player_state = PlayerState()
-
-r = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
-
-REDIS_KEY = "player_state"
-
-def save_state(state: PlayerState):
-    """Zapisuje stan gracza do Redis"""
-    r.set(REDIS_KEY, state.json())
-
-
-def load_state() -> PlayerState:
-    """Ładuje stan gracza z Redis"""
-    data = r.get(REDIS_KEY)
-    if data:
-        return PlayerState.parse_raw(data)
-    default_state = PlayerState()
-    save_state(default_state)
-    return default_state
-
+service = PlayerStateService()
 
 @router.get("/player/state", response_model=PlayerState)
 def get_player_state():
     """Zwraca aktualny stan gracza"""
-    return load_state()
-
+    return service.load_state()
 
 @router.post("/player/reset", response_model=PlayerState)
 def reset_player_state():
     """Resetuje stan gracza do wartości domyślnych"""
-    new_state = PlayerState()
-    save_state(new_state)
-    return new_state
-
+    return service.reset_state()
 
 @router.post("/player/update", response_model=PlayerState)
-def update_player_state(updated_state: PlayerState):
+def update_player_state(updated: PlayerStateUpdate):
     """Aktualizuje stan gracza na podstawie danych wejściowych"""
-    save_state(updated_state)
-    return updated_state
+    current_state = service.load_state()
+    updated_data = updated.dict(exclude_unset=True)
+    new_state = current_state.copy(update=updated_data)
+    return service.update_state(new_state)
