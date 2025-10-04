bfe9fc2f433785416cee562ea213c32d2354c198
mkmak2 <mkmak.code@gmail.com>
2025-10-05 00:52:12 +0200

    Add test connection

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.player_state.router import router as player_router
 from app.simulation.router import router as simulation_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Life Simulation API",
@@ -10,6 +11,23 @@ app = FastAPI(
 
 app.include_router(player_router)
 app.include_router(simulation_router)
+
+# Dodajemy CORS, żeby frontend mógł odbierać dane z innego portu
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/hello")
+async def hello():
+    return {"message": "Hello World"}
 
 @app.get("/")
 def root():
