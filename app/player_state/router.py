7c79137ec2d5fa896731c94f02dd1055b19d4906
bindasp <bindas.patryk@gmail.com>
2025-10-05 08:11:49 +0200

    added calculate_pension

--- app/player_state/router.py
+++ app/player_state/router.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
 from app.player_state.schema import PlayerStateUpdate
 from app.player_state.models import PlayerState
 from app.player_state.services import PlayerStateService
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.database import get_db
 
 router = APIRouter()
 service = PlayerStateService()
@@ -20,3 +22,18 @@ def update_player_state(updated: PlayerStateUpdate):
     updated_data = updated.dict(exclude_unset=True)
     new_state = current_state.copy(update=updated_data)
     return service.update_state(new_state)
+
+
+@router.post("/player/pension", response_model=float)
+def calculate_pension_endpoint():
+
+    state = service.load_state()
+
+    if not state:
+        raise HTTPException(status_code=404, detail="Stan gracza nie został znaleziony")
+
+    expected_lifetime_months = 264,2
+    if state.sex=="Mężczyzna":
+        expected_lifetime_months = 218.9
+    pension = state.calculate_pension(expected_lifetime_months)
+    return pension
